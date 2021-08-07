19c0c7d977ddd81a2acf423bdde358ed767341f9
Wingman <ryuash123@gmail.com>
2021-08-07 14:47:04 +0800

    fix: about

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,7 @@ class About extends Component {
     if (!this.props.data) return null;
 
     const about = this.props.data;
+    const social = this.props.data.social;
 
     return (
       <section id="about" className="section-padding">
@@ -29,17 +30,35 @@ class About extends Component {
             </div>
             <h1>{"About"}</h1>
             <div className="twelve columns main-col">
-              <p className="about-text text-center">{about.bio_intro}</p>
+              <p className="about-text text-center no-margin-bottom">{about.bio_intro}</p>
+              <p className="about-text text-center">{about.bio_intro_1}</p>
               {/* <p className="about-text">{about.bio_mission}</p> */}
-              <p className="about-text text-center">{about.bio_mission_description}</p>
-              <p className="about-text text-center">{about.bio_mission_no_cat_litter}</p>
-              <p className="about-text text-center">{about.bio_support_us}</p>
-              {/* <p className="about-text">{about.bio_stay_pawsome}</p>
-              <p className="about-text">{about.bio_cat_boss}</p>
-              <p className="about-text">{about.bio_ryuuash}</p>
-              <p className="about-text">{about.bio_mandoka}</p>
-              <p className="about-text">{about.bio_nayn}</p>
-              <p className="about-text">{about.bio_ps}</p> */}
+              <p className="about-text">{about.bio_mission_intro}</p>
+              <p className="about-text">{about.bio_mission_description_intro}</p>
+              <p className="about-text">Our goal?</p>
+              <p className="about-text">{about.bio_mission_description}</p>
+              <p className="about-text"><b>{about.bio_mission_no_cat_litter}</b></p>
+              <div className="socials">
+              {social.map((x, i) => (
+                <>
+                  <p className="social">
+                    <a href={x.url} key={x.url}>
+                        {x.name}
+                    </a>
+                  </p>
+                  {i !== social.length - 1 && (
+                    <p className="spacer">|</p>
+                  )}
+                  </>
+              ))}
+              </div>
+              {/* <p className="about-text">{about.bio_support_us}</p> */}
+              {/* <p className="about-text text-center">{about.bio_stay_pawsome}</p> */}
+              {/* <p className="about-text">{about.bio_cat_boss}</p> */}
+              {/* <p className="about-text">{about.bio_ryuuash}</p> */}
+              {/* <p className="about-text">{about.bio_mandoka}</p> */}
+              {/* <p className="about-text">{about.bio_nayn}</p> */}
+              {/* <p className="about-text">{about.bio_ps}</p> */}
               {/* <div className="cat-pics">
               <img
                 className="about-cat-pic"
