150a316f27a1b2dc1ac1d0eeb9254990dd1e06e4
Magic Cat <37407870+MonikaCat@users.noreply.github.com>
2021-08-25 00:08:29 +0100

    Updated images to svg format (#17)
    * Updated images to svg format
    
    * Update favicon
    
    * Code cleanup

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,32 +7,19 @@ class About extends Component {
 
     const about = this.props.data;
     const social = this.props.data.social;
+    const logo = this.props.data.logo;
 
     return (
       <section id="about" className="section-padding">
         <Fade duration={1000}>
           <div className="row">
             <div className="cat-pics">
-              <img src="/images/catboss.png" className="catboss-logo" alt="catboss-logo"></img>
-            {/* <img
-              className="cat-pic"
-              src="/images/cat-blue.png"
-              alt="Meow"
-            /><img
-              className="cat-pic"
-              src="/images/cat-pink.png"
-              alt="Meow"
-            /><img
-              className="cat-pic"
-              src="/images/cat-grey.png"
-              alt="Meow"
-            /> */}
+              <img src={logo} alt="catboss-logo" className="catboss-logo" />
             </div>
             <h1>{"About"}</h1>
             <div className="twelve columns main-col">
               <p className="about-text text-center no-margin-bottom">{about.bio_intro}</p>
               <p className="about-text text-center">{about.bio_intro_1}</p>
-              {/* <p className="about-text">{about.bio_mission}</p> */}
               <p className="about-text">{about.bio_mission_intro}</p>
               <p className="about-text">{about.bio_mission_description_intro}</p>
               <p className="about-text">Our goal?</p>
@@ -52,20 +39,6 @@ class About extends Component {
                   </>
               ))}
               </div>
-              {/* <p className="about-text">{about.bio_support_us}</p> */}
-              {/* <p className="about-text text-center">{about.bio_stay_pawsome}</p> */}
-              {/* <p className="about-text">{about.bio_cat_boss}</p> */}
-              {/* <p className="about-text">{about.bio_ryuuash}</p> */}
-              {/* <p className="about-text">{about.bio_mandoka}</p> */}
-              {/* <p className="about-text">{about.bio_nayn}</p> */}
-              {/* <p className="about-text">{about.bio_ps}</p> */}
-              {/* <div className="cat-pics">
-              <img
-                className="about-cat-pic"
-                src="/images/cat.png"
-                alt="Meow"
-              />
-              </div> */}
             </div>
           </div>
         </Fade>
--- src/Components/Team.js
+++ src/Components/Team.js
@@ -12,19 +12,6 @@ class About extends Component {
         <Fade duration={1000}>
           <div className="row">
             <div className="cat-pics">
-            {/* <img
-              className="cat-pic"
-              src="/images/cat-blue.png"
-              alt="Meow"
-            /><img
-              className="cat-pic"
-              src="/images/cat-pink.png"
-              alt="Meow"
-            /><img
-              className="cat-pic"
-              src="/images/cat-grey.png"
-              alt="Meow"
-            /> */}
             </div>
             <h1 className="title">{"MY PAWMATES"}</h1>
             <p className="text-center">{team.description}</p>
@@ -40,19 +27,6 @@ class About extends Component {
                   <p className="text-center">{x.bio}</p>
                 </div>
               ))}
-              {/* <p className="about-text">{about.bio_stay_pawsome}</p>
-              <p className="about-text">{about.bio_cat_boss}</p>
-              <p className="about-text">{about.bio_ryuuash}</p>
-              <p className="about-text">{about.bio_mandoka}</p>
-              <p className="about-text">{about.bio_nayn}</p>
-              <p className="about-text">{about.bio_ps}</p>
-              <div className="cat-pics">
-              <img
-                className="about-cat-pic"
-                src="/images/cat.png"
-                alt="Meow"
-              />
-              </div> */}
             </div>
           </div>
         </Fade>
